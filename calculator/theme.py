e3ee737dd02e1344cc6515f10688f0004abae180
victor <victorhugoocordeirorocha@gmail.com>
2024-06-15 20:59:58 -0400

    Removendo alguns icones

--- calculator/theme.py
+++ calculator/theme.py
@@ -6,9 +6,7 @@ ROOT_DIR = Path(__file__).parent
 # Caminho da pasta do meus icones
 FILE_DIR = ROOT_DIR / 'icons'
 # Caminho do arquvio em si
-WINDOW_ICON_PATH_OM = FILE_DIR / 'icon.png'
 WINDOW_ICON_PATH_CALCULATOR = FILE_DIR / 'calculator_icon.png'
-WINDOW_ICON_PATH_GOOGLE = FILE_DIR / 'calculate_icon_google.png'
 
 # Colors
 PRIMARY_COLOR = '#215fa2'
